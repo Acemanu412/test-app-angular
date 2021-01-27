d2f0f606a81751712c1eb16433c9ecdcd70e300e
Manu Aceituno <manuel.n.aceituno@gmail.com>
2021-01-26 17:20:42 -0800

    Add In-App Navigation. TODO: Get Data from a Server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,12 @@ export class HeroService {
     this.MessageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  
+
+  getHero(id: number): Observable<Hero | undefined> {
+    // TODO: send the message _after_ fetching the hero
+    this.MessageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   constructor(private MessageService: MessageService) { }
 }
